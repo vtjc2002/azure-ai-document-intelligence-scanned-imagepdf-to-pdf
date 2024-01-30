31ccc638dbc287b82a8f35db80744d81b4ae61a5
Johnson Tseng <vtjc2002@gmail.com>
2024-01-30 16:59:42 -0500

    update readme.md and refactor function code

--- README.md
+++ README.md
@@ -1,4 +1,93 @@
-# azure-ai-document-intelligence-scanned-imagepdf-to-pdf
+# Azure Document Intelligence SDK with Blob Storage Event Trigger
 
-https://learn.microsoft.com/en-us/azure/azure-functions/functions-event-grid-blob-trigger?tabs=isolated-process%2Cnodejs-v4&pivots=programming-language-csharp
-https://techcommunity.microsoft.com/t5/ai-azure-ai-services-blog/generate-searchable-pdfs-with-azure-form-recognizer/ba-p/3652024
+This project uses the Azure Document Intelligence SDK to read an image with the `prebuilt-document` model from a Blob Storage event trigger and write the results into {filename}-page-{x}.txt into the configured blob container.
+
+## Infrastructure
+Follow the below steps to provision necessary Azure infrastructure for this code sample to work using az cli and powershell.
+
+___Please note that you should secure your network using Vnet and secrets using Azure KeyVault for production purpose.___
+
+```powershell
+# set ps variables
+$randomNum = Get-Random -Minimum 100 -Maximum 999
+$appName = "docintellpoc"+$randomNum
+$location = "eastus"
+$resourceGroup = "rg-"+ $appName
+$scannedImageContainer = "scanned-images"
+$processedContainer = "parased-txt"
+
+# create resource group
+az group create --name $resourceGroup --location $location
+
+# create Storage account where the image will be uploaded
+az storage account create --name "sa$appName" --resource-group $resourceGroup --location $location --sku Standard_LRS --allow-blob-public-access false
+
+# create scanned-images container in that storage account
+$storageConnstr=$(az storage account show-connection-string --name "sa$appName" --resource-group $resourceGroup --output tsv --query connectionString)
+
+az storage container create --name $scannedImageContainer --connection-string $storageConnstr
+
+# create scanned-images container in that storage account
+az storage container create --name $processedContainer --connection-string $storageConnstr
+
+# create Document Intelligence Service (Single-Service)
+az cognitiveservices account create --name "ai-di-$appName" --resource-group $resourceGroup --kind FormRecognizer --sku S0 --location $location --assign-identity --custom-domain "ai-di-$appName"  --yes
+
+# create Storage account for Azure Function
+az storage account create --name "safunc$appName" --resource-group $resourceGroup --location $location --sku Standard_LRS --allow-blob-public-access false 
+
+# create Azure Function
+az functionapp create --consumption-plan-location $location --name "${appName}func" --os-type Windows --resource-group $resourceGroup --functions-version 4 --runtime dotnet-isolated --runtime-version 8 --storage-account "safunc$appName" 
+
+```
+
+Now we will configure Azure Function Application Settings
+
+```ps
+# set value for ImageSourceStorage
+$imageSourceStorageConnectionString= $(az storage account show-connection-string --name "sa$appName" --resource-group $resourceGroup --output tsv --query connectionString)
+
+az functionapp config appsettings set --name "${appName}func" --resource-group $resourceGroup --settings "ImageSourceStorage=$imageSourceStorageConnectionString"
+
+# set value for ProcessedContainer this is the container where the processed file is storaged as filename-{txt}-{pageNumber}.txt
+
+az functionapp config appsettings set --name "${appName}func" --resource-group $resourceGroup --settings "ProcessedContainer=$processedContainer"
+
+# set value for Azure Document Intelligence endpoint
+$docIntlEndpoint = $(az cognitiveservices account show --name "ai-di-$appName" --resource-group $resourceGroup --output tsv --query properties.endpoint)
+
+az functionapp config appsettings set --name "${appName}func" --resource-group $resourceGroup --settings "AzureDocumentIntelligenceEndpoint=$docIntlEndpoint"
+
+# set value for Azure Document Intelligence endpoint
+$docIntlKey = $(az cognitiveservices account keys list --name "ai-di-$appName" --resource-group $resourceGroup --output tsv --query key1)
+
+az functionapp config appsettings set --name "${appName}func" --resource-group $resourceGroup --settings "AzureDocumentIntelligenceKey=$docIntlKey"
+
+```
+
+## Setup
+
+1. Clone this repository to your local machine.
+2. Open the project in your preferred IDE.
+
+## How to use
+
+1. Upload an image to your Blob Storage account. This will trigger the Azure Function.
+
+2. The Azure Function is set up to use the Azure Document Intelligence SDK to read the image with the `prebuilt-document` model.
+
+3. The result of the read operation will be stored in the same storage account where the image is uploaded
+
+## Azure Function
+
+The Azure Function is triggered by a Blob Storage event. When a new image is uploaded to the Blob Storage, the function is triggered and the image is processed using the Azure Document Intelligence SDK.
+
+The function uses the `prebuilt-document` model to read the image. This model is designed to extract pages and paragraphs from documents.
+
+## Azure Document Intelligence SDK
+
+The Azure Document Intelligence SDK is used to read the image. It uses the `prebuilt-document` model to extract pages and paragraphs from the image.
+
+## Next steps
+
+You can customize the Azure Function and the use of the Azure Document Intelligence SDK to suit your needs. For example, you can choose a different model to read the image, or you can modify the function to perform additional tasks after reading the image.
--- src/ImageToPdf/ImageToPdfFunc.cs
+++ src/ImageToPdf/ImageToPdfFunc.cs
@@ -1,10 +1,7 @@
-using System.IO;
 using System.Threading.Tasks;
 using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
-using Azure.Storage;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -13,13 +10,15 @@ namespace ImageToPdf
     public class ImageToPdfFunc
     {
         private readonly ILogger<ImageToPdfFunc> _logger;
-        private readonly string _scannedImagesContainerName = "scanned-images";
 
         public ImageToPdfFunc(ILogger<ImageToPdfFunc> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Azure Function triggered by a blob event.  The blob is analyzed using Azure Document Intelligence and the result is written to a txt file in the configured ProcessedContainer container
+        /// </summary>
         [Function("ImageToPdfFuncBlobEventTrigger")]
         public async Task ImageToPdfBlobEventTrigger([BlobTrigger("scanned-images/{name}", Source = BlobTriggerSource.EventGrid, Connection = "ImageSourceStorage")] Stream inputStream, string name)
         {
@@ -32,33 +31,40 @@ namespace ImageToPdf
             var credential = new AzureKeyCredential(docIntelKey);
             var client = new DocumentAnalysisClient(new Uri(docIntelEndpoint), credential);
 
-            // convert the stream to a memory stream.  This is needed as the AnalyzeDocumentAsync method requires a seekable stream
-            using var ms = new MemoryStream();
-            inputStream.CopyTo(ms);
-            ms.Position = 0;
+            try
+            {
 
+                // convert the stream to a memory stream.  This is needed as the AnalyzeDocumentAsync cannot read diretly from the blob uri that's not publicly accessible
+                using var ms = new MemoryStream();
+                inputStream.CopyTo(ms);
+                ms.Position = 0;
 
-            AnalyzeDocumentOperation operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, modelId: "prebuilt-document", document: ms);
-            AnalyzeResult result = operation.Value;
+                // analyze the document with prebuilt-document model
+                AnalyzeDocumentOperation operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, modelId: "prebuilt-document", document: ms);
+                AnalyzeResult result = operation.Value;
 
-            // process the AnalyzeResult
-            await ProcessAnalyzeResult(result, name);
+                // process the AnalyzeResult
+                await ProcessAnalyzeResult(result, name);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error analyzing document: {ex.Message}");
+            }
 
-            ms.Dispose();
+            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} ");
 
-            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data:");
+            //TODO: Notifiy the user that the document has been processed.  
+            //The recommendation is to use Azure Event Grid.  See https://docs.microsoft.com/en-us/azure/event-grid/overview
         }
 
         /// <summary>
-        /// Process the AnalyzeResult and group paragraphs by page and use stringbuilder to create a string for each page
+        /// Process the AnalyzeResult and group paragraphs by page and use stringbuilder to create a string for each page.  The result is then written to a txt file in the configured ProcessedContainer container
         /// </summary>
-        /// <param name="result"></param>
-        /// <param name="filename"></param>
+        /// <param name="result">result from DocumentAnalysis</param>
+        /// <param name="filename">file name</param>
         /// <returns></returns>
         private async Task ProcessAnalyzeResult(AnalyzeResult result, string filename)
         {
-            // create a stringbuilder to hold the text for each page
-            var sb = new System.Text.StringBuilder();
 
             // group paragraphs by page
             var pageResults = result.Paragraphs.GroupBy(g => g.BoundingRegions[0].PageNumber).Select(s => new { PageNumber = s.Key, Paragraphs = s.Select(sm => sm.Content) });
@@ -66,29 +72,27 @@ namespace ImageToPdf
             // loop through each page
             for (int i = 0; i < pageResults.Count(); i++)
             {
+                // create a stringbuilder to hold the text for each page
+                var sb = new System.Text.StringBuilder();
+
                 // loop through each paragraph on the page and add it to the stringbuilder
                 foreach (var paragraph in pageResults.ElementAt(i).Paragraphs)
                 {
                     sb.AppendLine(paragraph);
                 }
 
-                try { 
-                    // write the stringbuilder to a txt file
-                    await WriteAnalyzeResultToBlob(sb.ToString(), $"{filename}-page-{i + 1}");
-                }
-                catch (RequestFailedException ex)
-                {
-                    _logger.LogError($"Error writing to blob: {ex.Message}");
-                }
+                // write the stringbuilder to a txt file
+                await WriteAnalyzeResultToBlob(sb.ToString(), $"{filename}-page-{i + 1}");
 
             }
+
         }
 
         /// <summary>
-        /// Write teh AnalyzeResult to txt files in the parsed-text container
+        /// Write teh AnalyzeResult to txt files in the configured ProcessedContainer container
         /// </summary>
-        /// <param name="result"></param>
-        /// <param name="filename"></param>
+        /// <param name="result">plain text to output</param>
+        /// <param name="filename">file name</param>
         /// <returns></returns>
         private async Task WriteAnalyzeResultToBlob(string result, string filename)
         {
@@ -107,19 +111,25 @@ namespace ImageToPdf
             // get a reference to the blob
             var blobClient = containerClient.GetBlobClient($"{filename}.txt");
 
+            using (var ms = new MemoryStream())
+            using (var sw = new StreamWriter(ms))
+            {
+                sw.Write(result);
+                sw.Flush();
+                ms.Position = 0;
 
-            // convert result to a memory stream
-            using var ms = new MemoryStream();
-            using var sw = new StreamWriter(ms);
+                // try writing the text to the blob
+                try
+                {
+                    await blobClient.UploadAsync(content: ms, overwrite: true);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error writing to blob: {ex.Message}");
+                }
 
-            sw.Write(result);
-            sw.Flush();
-            ms.Position = 0;
+            }
 
-            // write the text to the blob
-            await blobClient.UploadAsync(content: ms, overwrite: true);
-
-            
         }
     }
 }
