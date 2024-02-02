6053ee15c667bac4b7ffa3078a3594d10d3b7d2f
Johnson Tseng <vtjc2002@gmail.com>
2024-02-02 18:44:59 -0500

    refactor to handle words in same line

--- src/ImageToPdf/ImageToPdfFunc.cs
+++ src/ImageToPdf/ImageToPdfFunc.cs
@@ -1,9 +1,17 @@
+using System.Drawing;
 using System.Threading.Tasks;
+
 using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Azure.Storage.Blobs;
+
+using GrapeCity.Documents.Pdf;
+using GrapeCity.Documents.Text;
+
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+
+
 
 namespace ImageToPdf
 {
@@ -66,8 +74,20 @@ namespace ImageToPdf
         private async Task ProcessAnalyzeResult(AnalyzeResult result, string filename)
         {
 
+            // process the paragraphs and save to txt in the ProcessedTxtContainer
+            await ProcessAnalyzeResultParagraphsToTxtFiles(result.Paragraphs, filename);
+
+            // process the pages and save to PDF in the ProcessedPdfContainer
+            await ProcessAnalyzeReultPagesToPdf(result.Pages, filename);
+
+        }
+
+        private async Task ProcessAnalyzeResultParagraphsToTxtFiles(IReadOnlyList<DocumentParagraph> result, string filename)
+        {
+            var containerName = Environment.GetEnvironmentVariable("ProcessedTxtContainer");
+
             // group paragraphs by page
-            var pageResults = result.Paragraphs.GroupBy(g => g.BoundingRegions[0].PageNumber).Select(s => new { PageNumber = s.Key, Paragraphs = s.Select(sm => sm.Content) });
+            var pageResults = result.GroupBy(g => g.BoundingRegions[0].PageNumber).Select(s => new { PageNumber = s.Key, Paragraphs = s.Select(sm => sm.Content) });
 
             // loop through each page
             for (int i = 0; i < pageResults.Count(); i++)
@@ -81,20 +101,79 @@ namespace ImageToPdf
                     sb.AppendLine(paragraph);
                 }
 
-                // write the stringbuilder to a txt file
-                await WriteAnalyzeResultToBlob(sb.ToString(), $"{filename}-page-{i + 1}");
+                // write the stringbuilder to memory stream
+                using (var ms = new MemoryStream())
+                using (var sw = new StreamWriter(ms))
+                {
+                    sw.Write(sb.ToString());
+                    sw.Flush();
+                    ms.Position = 0;
 
+                    // write the stringbuilder to a txt file
+                    await WriteAnalyzeResultToBlob(ms, containerName, $"{filename}-page-{i + 1}.txt");
+                }
+            }
+        }
+
+        private async Task ProcessAnalyzeReultPagesToPdf(IReadOnlyList<DocumentPage> result, string filename)
+        {
+            // get the processed pdf container name from the environment variable
+            var containerName = Environment.GetEnvironmentVariable("ProcessedPdfContainer");
+
+            // Create a new PDF document:
+            GcPdfDocument doc = new GcPdfDocument();
+
+            // By default, DsPdf uses 72dpi:
+            const float In = 72;
+
+            // Loop through each page
+            foreach (var page in result)
+            {
+                // Create a new page in the PDF document
+                var pdfPage = doc.NewPage();
+                // Set page size bsed on the page size from the DocumentAnalysis result
+                pdfPage.Size = new SizeF((float)(page.Width * In), (float)(page.Height * In));
+
+                // Create a new graphics object to draw on the page
+                GcPdfGraphics g = pdfPage.Graphics;
+
+                // Loop through each word on the page
+                foreach (var word in page.Words)
+                {
+                    // Calculate optimal font size based on the polygon
+                    var desiredTextHeight = word.BoundingPolygon[3].Y - word.BoundingPolygon[0].Y;
+
+                    // Set Font and FontSize
+                    var tf = new TextFormat() { Font = StandardFonts.Times, FontSize = In * (float)desiredTextHeight, FontSizeInGraphicUnits = true };
+
+                    var tl = g.CreateTextLayout();
+                    tl.Append(word.Content, tf);
+                    tl.PerformLayout(true);
+
+                    PointF pt = new PointF(word.BoundingPolygon[0].ToVector2());
+                    g.DrawTextLayout(tl, pt);
+                }
+            }
+
+            // Save the PDF to a memory stream
+            using (var ms = new MemoryStream())
+            {
+                doc.Save(ms);
+                ms.Position = 0;
+
+                // write the PDF to a blob
+                await WriteAnalyzeResultToBlob(ms, containerName, $"{filename}.pdf");
             }
 
         }
 
         /// <summary>
-        /// Write teh AnalyzeResult to txt files in the configured ProcessedContainer container
+        /// Write 
         /// </summary>
         /// <param name="result">plain text to output</param>
         /// <param name="filename">file name</param>
         /// <returns></returns>
-        private async Task WriteAnalyzeResultToBlob(string result, string filename)
+        private async Task WriteAnalyzeResultToBlob(MemoryStream ms, string? containerName, string fileName)
         {
             // get Azure Storage connection string from environment variable
             var storageConnectionString = System.Environment.GetEnvironmentVariable("ImageSourceStorage");
@@ -103,33 +182,26 @@ namespace ImageToPdf
             var blobServiceClient = new BlobServiceClient(storageConnectionString);
 
             // get a reference to the container
-            var containerClient = blobServiceClient.GetBlobContainerClient(Environment.GetEnvironmentVariable("ProcessedContainer"));
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
 
             // create the container if it doesn't exist
             await containerClient.CreateIfNotExistsAsync();
 
             // get a reference to the blob
-            var blobClient = containerClient.GetBlobClient($"{filename}.txt");
+            var blobClient = containerClient.GetBlobClient($"{fileName}");
 
-            using (var ms = new MemoryStream())
-            using (var sw = new StreamWriter(ms))
+            // try writing the text to the blob
+            try
             {
-                sw.Write(result);
-                sw.Flush();
-                ms.Position = 0;
-
-                // try writing the text to the blob
-                try
-                {
-                    await blobClient.UploadAsync(content: ms, overwrite: true);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"Error writing to blob: {ex.Message}");
-                }
-
+                await blobClient.UploadAsync(content: ms, overwrite: true);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error writing to blob: {ex.Message}");
             }
 
         }
+
     }
+
 }
